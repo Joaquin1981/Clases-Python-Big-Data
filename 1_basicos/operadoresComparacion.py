7cf9186cf90a5fcf85cfbd948db4d911abd3b78f
Joaquin Navarro <joaquin.navarro.catalinas@outlook.es>
2026-04-17 10:42:16 +0200

    Operadores Matemáticos y de Comparación

---
+++ 1_basicos/operadoresComparacion.py
@@ -0,0 +1,18 @@
+numA = 18
+numB = 5
+
+# mayor que
+resultado = numA > numB # >=
+print('Mayor que:', resultado)
+
+# menor que
+resultado = numA < numB
+print('Menor que:', resultado)
+
+# igualdad
+resultado = numA == numB
+print("Igual que:", resultado)
+
+# distinto
+resultado = numA != numB
+
---
+++ 1_basicos/operadoresMatematicos.py
@@ -0,0 +1,34 @@
+numA = 31
+numB = 12
+
+# suma
+print(5+8) # 13
+print(numA+numB)
+
+# resta
+print(43 - 81)
+print(numB - numA)
+
+#multiplicación
+print(3*98)
+
+# división
+print(18/5)
+
+# potencia
+print(2**3)
+
+# division entera
+print(18//5)
+
+# operador del módulo es el resto de lo que queda de la división 
+print(18%5)
+
+# incremento / decremento / asignación
+numero = 2
+
+numero += 2 # numero = numero + 2
+numero -= 3 # numero = numero - 3
+numero += 1 # numero = numero + 1
+
+print(numero)
